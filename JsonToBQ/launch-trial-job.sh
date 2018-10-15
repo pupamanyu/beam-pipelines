03cc6c5a3b135c57c8ba53ef272c9332068b1f65
Pramod Rao <pramodrao@pramodrao-macbookpro.roam.corp.google.com>
2018-10-14 20:26:50 -0700

    Adding Launch Script

---
+++ JsonToBQ/launch-trial-job.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+# Script to Launch the pipeline for JsonToBQ
+#
+JOBJAR="<FULL PATH>/JsonToBQ-1.0-all.jar"
+PROJECT="<GCP PROJECT>"
+OUTPUTDATASET="<BQ DATASET>"
+OUTPUTTABLE="<BQ MAIN TABLENAME>"
+ERRORTABLE="<BQ ERROR TABLENAME>"
+# INPUT FILES are assumed to be in location <input bucket>/backup/topic=<topic name>/year=<year>/month=<month>/day=<day>/hour=<hour>
+INPUTBUCKET="<INPUT BUCKET NAME>"
+TOPIC="<topic name>"
+INPUTDIRECTORY="${INPUTBUCKET}/backup/topic=${TOPIC}"
+STAGINGBUCKET="<Dataflow Staging Regional Bucket Name>"
+STAGINGDIRECTORY="${STAGINGBUCKET}/staging"
+# Comma Separated Prefixes in the form of yyyy-MM-dd
+INPUTPREFIXES="<yyyy-MM-dd>,...."
+BQSCHEMAFILE="${STAGINGBUCKET}/${OUTPUTTABLE}_bq_schema.json"
+JSONSCHEMAFILE="${STAGINGBUCKET}/${OUTPUTTABLE}_json_schema.json"
+INPUTSUFFIX=".gz"
+TEMPDIRECTORY="${STAGINGBUCKET}/temp"
+RUNNER="DataFlow"
+JOBTAGPREFIX="bq-load"
+DISKSIZEGB=256
+WORKERMACHINETYPE="n1-standard-4"
+#MAXWORKERS=600
+SUBNETWORK="https://www.googleapis.com/compute/v1/projects/<host vpc project>/regions/<region name>/subnetworks/<subnetwork name>"
+AUTOSCALINGALGO="THROUGHPUT_BASED"
+OUTPUTPARTITIONCOLUMN="<OUTPUT PARTITION COLUMN NAME>"
+ERRORPARTITIONCOLUMN="<ERROR PARTITION COLUMN NAME>"
+TIMESTAMPCOLUMN="<TIMESTAMP COLUMN NAME>"
+PARTITIONCOLUMNDATEFORMAT="yyyy-MM-dd"
+# Comma Separated of PII Fields
+SENSITIVEFIELDS="<field1>,<field2>,..."
+GEOROOTFIELDNAME="<root field name for the geo object>"
+
+java \
+  -jar ${JOBJAR} \
+  --outputDatasetName=${OUTPUTDATASET} \
+  --outputTableName=${OUTPUTTABLE} \
+  --errorTableName=${ERRORTABLE} \
+  --outputTablePartitionColumn=${OUTPUTPARTITIONCOLUMN} \
+  --errorTablePartitionColumn=${ERRORPARTITIONCOLUMN} \
+  --timestampColumn=${TIMESTAMPCOLUMN} \
+  --partitionColumnDateFormat=${PARTITIONCOLUMNDATEFORMAT} \
+  --sensitiveFields=${SENSITIVEFIELDS} \
+  --geoRootFieldName=${GEOROOTFIELDNAME} \
+  --inputDirectory=${INPUTDIRECTORY} \
+  --project=${PROJECT} \
+  --subnetwork=${SUBNETWORK} \
+  --diskSizeGb=${DISKSIZEGB} \
+  --workerMachineType=${WORKERMACHINETYPE} \
+  --autoscalingAlgorithm=${AUTOSCALINGALGO} \
+  --inputPrefixes=${INPUTPREFIXES} \
+  --BQSchemaFile=${BQSCHEMAFILE} \
+  --JSONSchemaFile=${JSONSCHEMAFILE} \
+  --inputFilenameSuffix=${INPUTSUFFIX} \
+  --tempLocation=${TEMPDIRECTORY} \
+  --stagingLocation=${STAGINGDIRECTORY} \
+  --runner=${RUNNER} \
+  --jobTagPrefix=${JOBTAGPREFIX}
